7407db0ee12861b3ab521c898dc656961a3bc34d
Maike Rosa da Silva <maikeofc18@gmail.com>
2022-12-01 14:02:29 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from requests.auth import HTTPBasicAuth
 from getpass import getpass
 
 username = 'maikerosa'
-token = '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'
+token = ''
 
 response = requests.get(f'https://api.github.com/users/maikerosa/following/maikerosa', auth=HTTPBasicAuth(username, token))
 
